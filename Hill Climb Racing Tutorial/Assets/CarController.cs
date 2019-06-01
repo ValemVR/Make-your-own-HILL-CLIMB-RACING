6133b81927c90985dcc19fb1e9ca76e7741cca10
Valem <qvalem.vr@gmail.com>
2019-06-01 22:33:59 +0200

    first commit

---
+++ Hill Climb Racing Tutorial/Assets/CarController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+
+    [HideInInspector]
+    public float fuel = 1;
+    public float fuelconsumption = 0.1f;
+    public Rigidbody2D carRigidbody;
+    public Rigidbody2D backTire;
+    public Rigidbody2D frontTire;
+    public float speed = 20;
+    public float carTorque = 10;
+    private float movement;
+    public UnityEngine.UI.Image image;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        movement = Input.GetAxis("Horizontal");
+        image.fillAmount = fuel;
+	}
+
+    private void FixedUpdate()
+    {
+        if(fuel>0)
+        {
+            backTire.AddTorque(-movement * speed * Time.fixedDeltaTime);
+            frontTire.AddTorque(-movement * speed * Time.fixedDeltaTime);
+            carRigidbody.AddTorque(-movement * carTorque * Time.fixedDeltaTime);
+        }
+
+        fuel -= fuelconsumption * Mathf.Abs(movement) * Time.fixedDeltaTime;
+    }
+}
